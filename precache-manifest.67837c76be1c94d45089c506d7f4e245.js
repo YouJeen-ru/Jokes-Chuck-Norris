823b151a65833531808720743beac6e9000d8014
YouJeen-ru <1.rouch@mail.ru>
2020-10-16 12:28:11 +0300

    Updates

---
+++ precache-manifest.67837c76be1c94d45089c506d7f4e245.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f11247c4238b015c4f6a51baed7b861",
+    "url": "/Jokes-Chuck-Norris/index.html"
+  },
+  {
+    "revision": "d2652618f8b2122302b4",
+    "url": "/Jokes-Chuck-Norris/static/css/main.9198eaa0.chunk.css"
+  },
+  {
+    "revision": "07d823f68b096e5aebf8",
+    "url": "/Jokes-Chuck-Norris/static/js/2.7b21890b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Jokes-Chuck-Norris/static/js/2.7b21890b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2652618f8b2122302b4",
+    "url": "/Jokes-Chuck-Norris/static/js/main.fb9a1a3d.chunk.js"
+  },
+  {
+    "revision": "ef7407ac90ace152e359",
+    "url": "/Jokes-Chuck-Norris/static/js/runtime-main.1ba6868e.js"
+  }
+]);

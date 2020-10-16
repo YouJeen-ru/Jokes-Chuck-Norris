25699cec0f3a1c815fb537e157fc79382d07a6ad
YouJeen-ru <1.rouch@mail.ru>
2020-10-16 12:16:15 +0300

    new update app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+
 
 const API_URL = `http://api.icndb.com/jokes/random`;
 
 function App() {
+
+    const [joke, setJoke] = useState('')
+
+    const generateJoke = () => {
+        fetch(API_URL)
+            .then(res => res.json())
+            .then(data => setJoke(data.value.joke))
+    }
+
+    useEffect(() => {
+        generateJoke()
+
+    }, [])
+
+
     return (
         <div className="box">
-            <h3>Chuck Norris Jokes Generator</h3>
-            <p>Here goes the joke</p>
-            <button>Get new joke ^_^</button>
+            <h2>Chuck Norris Jokes Generator</h2>
+            <p dangerouslySetInnerHTML={{__html: joke}}/>
+            <button onClick={generateJoke}>Get new joke :D</button>
         </div>
     );
 }
